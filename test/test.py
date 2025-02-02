8e20cac0ada9bf5fc633ba101a19052fcdb2c289
FJiuY <nophynolife@outlook.com>
2025-02-02 17:23:47 +0800

    Update readme.md. Add MIT License and requirements.txt. Ignore test folder and remove it from Git tracking

--- test/test.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout
-app = QApplication([])
-window = QWidget()
-layout = QHBoxLayout()
-layout.addWidget(QPushButton('Top'))
-layout.addWidget(QPushButton('Bottom'))
-window.setLayout(layout)
-window.show()
-app.exec()
